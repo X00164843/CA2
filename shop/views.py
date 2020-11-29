2793c3fea52b0cf0f1753c9698527a49250f8568
Darragh Murphy <x00162272@mytudublin.ie>
2020-11-29 15:02:34 +0000

    created views and templates for products and category

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+def allProdCat(request, category_id=None):
+    page = None
+    products = None
+    if category_id != None:
+        page = get_object_or_404(Category, id=category_id)
+        products = Product.objects.filter(category=page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+
+    return render(request,'shop/category.html',{'category':page, 'products':products})
+
+def prod_detail(request, category_id, product_id):
+    try:
+        product = Product.objects.get(category_id = category_id, id=product_id)
+    except Exception as e:
+        raise e
+    return render(request, 'shop/product.html', {'product':product})
